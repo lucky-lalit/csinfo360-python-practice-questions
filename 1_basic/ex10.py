32e0bdd144b73808812e9e9f6c89601e257273cf
lalit tiwari <ltiwari2005@gmail.com>
2025-07-22 00:52:51 +0530

    basics 9 and 10

--- 1_basic/ex10.py
+++ 1_basic/ex10.py
@@ -1,1 +1,7 @@
 # 10. Write a program to convert temperature from Fahrenheit to Celsius.
+
+inp_temp = float(input("Enter tmeperature in Fahrenheit: "))
+
+temp_cel = (inp_temp - 32) * 5 / 9
+
+print("Temperature in Celsius: ", temp_cel)
--- 1_basic/ex9.py
+++ 1_basic/ex9.py
@@ -1,1 +1,7 @@
 # 9. Write a program to convert temperature from Celsius to Fahrenheit.
+
+inp_temp = float(input("Enter the temperature in celcius: "))
+
+temp_Fahr = (inp_temp * 9 / 5) + 32
+
+print("Temperature in Fahrenheit: ", temp_Fahr)
