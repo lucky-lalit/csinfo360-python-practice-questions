c1999a81e279ca060e3d8db53a12192940709bea
lalit tiwari <ltiwari2005@gmail.com>
2025-07-21 23:22:29 +0530

    basics ex6

--- 1_basic/ex6.py
+++ 1_basic/ex6.py
@@ -1,1 +1,6 @@
 # 6. Write a program to input the basic salary of an employee and calculate the gross salary using standard allowances.
+
+basic_salary = input("Enter the Basic Salary: ")
+basic_salary = float(basic_salary)
+gross_salary = basic_salary + (basic_salary * 0.5) + (basic_salary * 0.15)
+print(gross_salary)
