42ca022a04758121f9408cffe86f59302828a3f1
lalit tiwari <ltiwari2005@gmail.com>
2025-07-21 23:36:59 +0530

    basics ex7

--- 1_basic/ex7.py
+++ 1_basic/ex7.py
@@ -1,1 +1,8 @@
 # 7. Write a program to calculate simple interest using principal, rate of interest, and time.
+
+inp_principle = float(input("Enter principle: "))
+inp_rate_interest = float(input("Enter rate of interest: "))
+inp_year = float(input("Enter the year: "))
+
+simple_interest = (inp_principle * (inp_rate_interest) * inp_year) / 100
+print("Simple Interest :", simple_interest)
