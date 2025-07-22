a6620594f38cb2d140a0468a7569eb220f5576da
lalit tiwari <ltiwari2005@gmail.com>
2025-07-22 23:33:56 +0530

    basics 11 done

--- 1_basic/ex11.py
+++ 1_basic/ex11.py
@@ -1,1 +1,8 @@
 # 11. Write a program to convert a distance in kilometers to both miles and meters.
+
+inp_dis = float(input("Enter the distance in kilometer: "))
+
+km_to_miles = inp_dis * 0.621371
+km_to_meters = inp_dis * 1000
+
+print("The Distance in miles", km_to_miles, "and in meters", km_to_meters)
