fa06cb82e5dbcfaf8b13c271490df8a5c7f417b1
lalit tiwari <ltiwari2005@gmail.com>
2025-07-29 00:45:03 +0530

    done with basic 24-25

--- 1_basic/ex24.py
+++ 1_basic/ex24.py
@@ -1,1 +1,8 @@
 # 24. Write a program to check whether a given number is even or odd.
+
+inp_num = int(input("Enter the number to check odd or even: "))
+
+if inp_num % 2 == 0:
+    print(f"{inp_num} is even")
+else:
+    print(f"{inp_num} is odd")
--- 1_basic/ex25.py
+++ 1_basic/ex25.py
@@ -1,1 +1,41 @@
 # 25. Write a program to input a character and determine whether it is a vowel or consonant.
+
+inp_char = str(input("Enter a character: "))
+lower_char = inp_char.lower()
+# print(lower_char)
+# char_list = []
+# char_list.append(inp_char)
+vowels = ["a", "e", "i", "o", "u"]
+for char in vowels:
+    if char == lower_char:
+        print(f"{inp_char} is vowel")
+        quit()
+    # else:
+print(f"{inp_char} is consonant")
+# break
+
+
+# if inp_char in vowels:
+#     print(f'{inp_char} is vowel')
+# else:
+#     print(f'{inp_char} is consonant')
+# if ch
+# print(inp_char == 'a' ,inp_char == 'e',1 or 2, 0 or 5,0 or 0 or False or [])
+# if inp_char == 'a' or inp_char == 'e' or inp_char == 'i' or inp_char == 'o' or inp_char == 'u':
+#     print(f'{inp_char} is vowel')
+# else:
+#     print(f'{inp_char} is consonant')
+
+
+# if inp_char == 'a':
+#     print(f'{inp_char} is vowel')
+# elif inp_char == 'e':
+#     print(f'{inp_char} is vowel')
+# elif inp_char == 'i':
+#     print(f'{inp_char} is vowel')
+# elif inp_char == 'o':
+#     print(f'{inp_char} is vowel')
+# elif inp_char == 'u':
+#     print(f'{inp_char} is vowel')
+# else:
+#     print(f'{inp_char} is consonant')
