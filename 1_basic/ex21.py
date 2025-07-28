f4f13077b065055bc66237031e761ff3bd06c6dd
lalit tiwari <ltiwari2005@gmail.com>
2025-07-28 23:51:19 +0530

    done with basic 21-22

--- 1_basic/ex21.py
+++ 1_basic/ex21.py
@@ -1,1 +1,7 @@
 # 21. Write a program to print all odd numbers from 1 up to a user-defined number n.
+
+num_inp = int(input("Enter a Number: "))
+
+for num in range(1, num_inp + 1):
+    if num % 2 == 1:
+        print(num)
--- 1_basic/ex22.py
+++ 1_basic/ex22.py
@@ -1,1 +1,7 @@
 # 22. Write a program to print all even numbers from 1 up to a user-defined number n.
+
+num_inp = int(input('Enter the Number: '))
+
+for num in range(1,num_inp+1):
+    if num % 2 == 0:
+        print(num)
