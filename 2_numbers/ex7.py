6c18a5c829addd742615a005fe856ac29b37ea5f
lalit tiwari <ltiwari2005@gmail.com>
2025-08-04 00:05:47 +0530

    done with numbers 7,9

--- 2_numbers/ex7.py
+++ 2_numbers/ex7.py
@@ -4,3 +4,17 @@
 # Example:
 # Input: 12 and 18
 # Output: 6
+
+inp_num1 = int(input("Enter the number to find HCF: "))
+inp_num2 = int(input("Enter the number to find HCF: "))
+i = 1
+highest = None
+# while i >= inp_num1 and i <= inp_num2:
+while i < inp_num2:
+    i = i + 1
+    if inp_num1 % i == 0 and inp_num2 % i == 0:
+        # print('debug1')
+        if (highest is None) or (highest < i):
+            highest = i
+            # print('debug2')
+print(highest)
--- 2_numbers/ex9.py
+++ 2_numbers/ex9.py
@@ -3,3 +3,12 @@
 # Example:
 # Input: 5
 # Output: 120 (5Ã—4Ã—3Ã—2Ã—1)
+
+import math
+
+inp_num = int(input("Enter the number to calculate the factorial: "))
+# print(math.factorial(inp_num))
+product = 1
+for i in range(1, inp_num + 1):
+    product = product * i
+print(product)
