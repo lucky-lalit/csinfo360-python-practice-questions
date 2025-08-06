50123dbf175d4f2ef200bdbc7e4a23b71b2f6d20
lalit tiwari <ltiwari2005@gmail.com>
2025-08-06 22:11:16 +0530

    done with numbers 32-33

--- 2_numbers/ex32.py
+++ 2_numbers/ex32.py
@@ -3,3 +3,14 @@
 # Example:
 # Input: 10 to 50
 # Output: 16, 25, 36, 49
+# import math
+inp_num_start = int(input("Enter the number from: "))
+inp_num_stop = int(input("Enter the number to: "))
+
+for i in range(inp_num_start, inp_num_stop + 1):
+    for j in range(i + 1):
+        if j * j == i:
+            print(j * j, end=" ")
+            break
+    # if i == math.sqrt(i)*math.sqrt(i):
+    # print(i)
--- 2_numbers/ex33.py
+++ 2_numbers/ex33.py
@@ -3,3 +3,18 @@
 # Example:
 # Input: 100 to 500
 # Output: 153, 370, 371, 407
+
+inp_num_start = int(input("Enter the number from: "))
+inp_num_stop = int(input("Enter the number to: "))
+# sum = 0
+for i in range(inp_num_start, inp_num_stop + 1):
+    # print(len(str(i)))
+    # print('debug 1',i)
+    sum = 0
+    for j in str(i):
+        # print('debug2',j)
+        # print(j,end=' ')
+        sum = sum + int(j) ** len(str(i))
+    # print('debug4',sum)
+    if i == sum:
+        print(i, end=" ")
