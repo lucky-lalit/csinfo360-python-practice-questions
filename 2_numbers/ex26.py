4e637ea4a54b410602f112d068bbcfc8e47c05d4
lalit tiwari <ltiwari2005@gmail.com>
2025-08-06 21:57:11 +0530

    done with numbers 26-27

--- 2_numbers/ex26.py
+++ 2_numbers/ex26.py
@@ -3,3 +3,14 @@
 # Example:
 # Input: 135
 # Output: Disarium (1Â¹ + 3Â² + 5Â³ = 135)
+
+inp_num = input("Enter the number: ")
+count = 0
+sum = 0
+for i in inp_num:
+    count = count + 1
+    sum = sum + int(i) ** count
+if sum == int(inp_num):
+    print("Disarium")
+else:
+    print("Not Disarium")
--- 2_numbers/ex27.py
+++ 2_numbers/ex27.py
@@ -3,3 +3,15 @@
 # Example:
 # Input: 42
 # Output: Pronic (6Ã—7=42)
+
+inp_num = int(input("Enter the NUmber: "))
+# product = 1
+# i =  0
+# while i < inp_num//2:
+#     i = i + 1
+for i in range(1, inp_num // 2):
+    # print('debug1',i,i+1,'=',i*(i+1))
+    if i * (i + 1) == inp_num:
+        print("Pronic")
+        quit()
+print("Not Pronic")
