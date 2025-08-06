c084d5a5f074458a68421a9b5ee6c2440f6e2d75
lalit tiwari <ltiwari2005@gmail.com>
2025-08-06 23:48:00 +0530

    done with numbers 44

--- 2_numbers/ex44.py
+++ 2_numbers/ex44.py
@@ -1,5 +1,23 @@
 ﻿# Exercise 44: Disarium Numbers in Range
 # Description: Write a program to find all Disarium numbers between a given range.
+# Description: Write a program to check if a number equals the sum of its digits raised to their respective positions.
 # Example:
 # Input: 1 to 200
 # Output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 89, 135, 175
+
+inp_num_start = int(input("Enter the number from: "))
+inp_num_stop = int(input("Enter the number to: "))
+
+for i in range(inp_num_start, inp_num_stop + 1):
+    sum = 0
+    # print('i',i)
+    power = 0
+    for j in str(i):
+        j = int(j)
+        # print('j',j)
+        # print('sum',sum)
+        power = power + 1
+        sum = sum + j**power
+        # print('sum_after',sum)
+    if sum == i:
+        print(i, end=" ")
