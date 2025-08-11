e89c3dab96fea99e204d116e6f7bf3467d4401a4
lalit tiwari <ltiwari2005@gmail.com>
2025-08-11 17:35:49 +0530

    done with number 59

--- 2_numbers/ex59.py
+++ 2_numbers/ex59.py
@@ -1,5 +1,37 @@
 ﻿# Exercise 59: Nth Spy Number
 # Description: Write a program to find the nth spy number.
+# Description: Write a program to check if the digit_sum of digits equals the product of digits.
 # Example:
 # Input: 5
 # Output: 5 (sequence: 1, 2, 3, 4, 5, 6, 7, 8, 9, 22, 123...)
+
+inp_num = int(input("Enter the nth number to get the nth spy number: "))
+
+
+def check_spy(num):
+    if num == 0:
+        return False
+    digit_sum = 0
+    product = 1
+    for i in str(num):
+        digit_sum = digit_sum + int(i)
+        product = product * int(i)
+    if digit_sum == product:
+        return True
+    else:
+        return False
+
+
+# print(check_spy(inp_num))
+
+i = 1
+count = 0
+nth_spy_num = None
+while count < inp_num:
+    if check_spy(i):
+        count = count + 1
+        nth_spy_num = i
+    i = i + 1
+print(nth_spy_num)
+
+# correct
